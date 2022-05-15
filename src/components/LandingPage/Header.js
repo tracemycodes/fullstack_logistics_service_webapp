881f1f37b5913f13e138826aeecccab8ac77f52c
Agba Emmanuel <agbaayoh@gmail.com>
2022-05-15 16:04:13 -0700

    navbar

--- src/components/LandingPage/Header.js
+++ src/components/LandingPage/Header.js
@@ -1,120 +1,127 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Btn, DivContainer } from '../../styles/styles'
+import React from 'react';
+import styled from 'styled-components';
+import { Btn, DivContainer } from '../../styles/styles';
 
 const NavBar = styled.header`
-  
-
   background-color: #002741;
-  
+`;
 
-`
+const NavHeader = styled(DivContainer)`
+  padding: 0.9rem 0.5rem;
+  // font-size: .75rem;
+  font-size: clamp(0.5rem, 3vw, 0.75rem);
+  display: flex;
+  justify-content: space-between;
+  color: #93afc1;
 
-const NavHeader = styled( DivContainer )`
-padding: .9rem .5rem;
-font-size: .75rem;
-display: flex;
-justify-content: space-between;
-color: #93AFC1;
-  
   a {
-    color: #93AFC1;
-    margin-right: .9rem;
-    font-size: .75rem;
+    color: #93afc1;
+    margin-right: 0.9rem;
+    font-size: 0.75rem;
     text-decoration: none;
-
   }
   span {
-    margin-left: .9rem;
+    margin-left: 0.9rem;
   }
-`
+`;
 
-const NavBody = styled( DivContainer )`
-  padding: .75rem .5rem;
-  font-size: .9rem;
+const NavBody = styled(DivContainer)`
+  padding: 0.75rem 0.5rem;
+  // font-size: .9rem;
+  font-size: clamp(0.5rem, 3vw, 0.9rem);
   display: flex;
   justify-content: space-between;
   align-items: center;
-  color: #93AFC1;
+  color: #93afc1;
 
   .product-logo {
     border: 2px solid green;
     width: 5rem;
     height: 2rem;
   }
-  
+
+  .nav-items {    
+    @media (max-width: 765px) {
+    display: none;
+    }
+  }
+
+  .nav-hamburger {
+    display: flex;
+    flex-direction: column;
+    div {
+      border: 2px solid black;
+      width: 2rem;
+      margin: .17rem;
+    }
+  }
+
   div {
     display: flex;
     align-items: center;
-    
+
     ul {
       display: flex;
 
       li {
         list-style: none;
         margin-left: 1.4rem;
-        font-size: .8rem;
-      }
+        // font-size: .8rem;
 
+        font-size: clamp(0.65rem, 2vw, 0.8rem);
+
+        
+      }
     }
   }
-  
-`
+`;
 
-const NavBtn = styled( Btn )`
+const NavBtn = styled(Btn)`
   margin-left: 3.5rem;
-  
-`
+  @media (max-width: 793px) {
+    // border: 2px solid black;
+    margin-left: 2rem;
+  }
+`;
 
 const Header = () => {
-
   return (
     <nav>
-        <NavBar>
-        <NavHeader>          
+      <NavBar>
+        <NavHeader>
           <div>
-            <a href="mailto:">mail@example.com</a>
-            <a href="tel:+">+145 (2466) 888</a>            
+            <a href='mailto:'>mail@example.com</a>
+            <a href='tel:+'>+145 (2466) 888</a>
           </div>
           <div>
-            <span>ENG</span>           
-            <span>FR</span>           
+            <span>ENG</span>
+            <span>FR</span>
           </div>
         </NavHeader>
       </NavBar>
-        <NavBody>
-          <div className="product-logo">
+      <NavBody>
+        <div className='product-logo'></div>
+        <div className="nav-items">
+          <ul>
+            <li>HOME</li>
+            <li>SERVICES</li>
+            <li>COMPANY</li>
+            <li>PAGES</li>
+            <li>NEWS</li>
+            <li>CONTACT</li>
+            <li>O</li>
+          </ul>
+          <NavBtn>GET QUOTE</NavBtn>
+        </div>
+        <div className="nav-hamburger">
+          <div className="first"></div>
+          <div className="second"></div>
+          <div className="third"></div>
+        </div>
+      </NavBody>
+      
+    </nav>
+  );
+};
 
-          </div>
-          <div>
-            <ul>
-              <li>
-                HOME
-              </li>
-              <li>
-                SERVICES
-              </li>
-              <li>
-                COMPANY
-              </li>
-              <li>
-                PAGES
-              </li>
-              <li>
-                NEWS
-              </li>
-              <li>
-                CONTACT
-              </li>
-              <li>
-                O
-              </li>
-            </ul>
-            <NavBtn>GET QUOTE</NavBtn>
-          </div>
-        </NavBody>
-      </nav>
-  )
-}
-
-export default Header
+export default Header;
