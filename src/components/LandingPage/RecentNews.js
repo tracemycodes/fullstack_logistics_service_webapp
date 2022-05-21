1272a776ab378a1fe64a849cc933899ae828d567
Agba Emmanuel <agbaayoh@gmail.com>
2022-05-21 06:24:58 -0700

    feat: improved slider

--- src/components/LandingPage/RecentNews.js
+++ src/components/LandingPage/RecentNews.js
@@ -2,38 +2,108 @@ import React from 'react'
 import styled from 'styled-components'
 import { DivContainer } from '../../styles/styles'
 
-const NewsSection = styled.section`
 
+const CarouselSection = styled(DivContainer)`
+// overflow-y: hidden;
+margin: auto;
+border: 2px solid red;
+`
+
+const NewsSection = styled.section`
+  color: ${({theme}) => theme.text_darkBlue};
+  text-align: center;
+  h2 {
+    margin: 6rem 0 1rem;
+  }
+  p {
+    margin: auto;
+    margin-bottom: 2rem;
+    width: 100%;
+    max-width: 35rem;
+  }
+  .news-scroll {
+  overflow-x: scroll;
+  overflow-y: hidden;
+    z-index: 2;
+    position: relative;
+    height: 100%;
+    
+  }
+  .news-bar {
+    width: 788px;
+    border: 2px solid blue;
+    display: block;
+
+      // height: 19rem;
+    // display: flex;
+    // // gap: 2rem;
+    // align-items: center;
+    // justify-content: space-between;
+
+    > * + *{
+      margin-left: 1.2rem;
+    }
+
+    article {
+      width: 15.5rem;
+      height: 19rem;
+      border: 2px solid green;
+      text-align: left;
+      display: inline-block;
+      .news-img {
+        height: 10rem;
+        border: 2px solid red;
+        margin-bottom: 1.5rem;
+      }
+      h3{
+        margin: 0 .5rem;
+        position: relative;
+        &:before {
+          content: " ";
+          position: absolute;
+          width: 2.7rem;
+          border: 1.8px solid red;
+          border-radius: 30px;
+          bottom: -0.4rem;
+          left: 0;
+        }
+      }
+      p {
+        margin: 2rem .5rem 1rem;
+      }
+    }
+  }
 `
 
 const RecentNews = () => {
   return (
     <NewsSection>
-      <DivContainer>
+      <CarouselSection>
         <h2>Recent News</h2>
         <p>Solving your supply chain needs from end to end, taking the complexity out of container shipping We are the forefront of developing innovation.</p>
+        
+        <div className="news-scroll">
         <div className="news-bar">
           <article>
             <div className="news-img"></div>
             <h3>Denso invests $10mn in india distribution</h3>
             <p className="news-text">By HARRY MENEAR - Mar 16, 2021 - 9:00AM</p>
           </article>
-        </div>
-        <div className="news-bar">
+        
+          <article>
+            <div className="news-img"></div>
+            <h3>Denso invests $10mn in india distribution</h3>
+            <p className="news-text">By HARRY MENEAR - Mar 16, 2021 - 9:00AM</p>
+          </article>
+       
           <article>
             <div className="news-img"></div>
             <h3>Denso invests $10mn in india distribution</h3>
             <p className="news-text">By HARRY MENEAR - Mar 16, 2021 - 9:00AM</p>
           </article>
         </div>
-        <div className="news-bar">
-          <article>
-            <div className="news-img"></div>
-            <h3>Denso invests $10mn in india distribution</h3>
-            <p className="news-text">By HARRY MENEAR - Mar 16, 2021 - 9:00AM</p>
-          </article>
         </div>
-      </DivContainer>
+      </CarouselSection>
     </NewsSection>
   )
 }
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,22 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const DivContainer = styled.div`
   max-width: 844px;
   width: 95%;
   margin: auto;
-  padding: 0 .8rem;
-
-`
+  padding: 0 0.8rem;
+`;
 
 export const Btn = styled.button`
-  padding: .65rem 1.6rem;
+  padding: 0.65rem 1.6rem;
   font-size: clamp(0.45rem, 2vw, 0.65rem);
-    box-shadow: 0px 5px 5px 3px rgba(0, 0, 0, 0.1);
-  background-color: #EC0101;
+  box-shadow: 0px 5px 5px 3px rgba(0, 0, 0, 0.1);
+  background-color: #ec0101;
   color: #fff;
   border: none;
   border-radius: 0.3rem;
   font-weight: bold;
   position: relative;
   z-index: 1;
-`
+`;
