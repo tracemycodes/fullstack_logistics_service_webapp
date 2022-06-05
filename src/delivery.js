6c8048d658344234eb0d1052a76fd471378c372c
Agba Emmanuel <agbaayoh@gmail.com>
2022-06-05 06:42:33 -0700

    about section info moved to obj

---
+++ src/delivery.js
@@ -0,0 +1,47 @@
+// this file hold all the data used as mock up and content for this application, make changes to get your custom design
+
+const contact = {
+  email: 'mail@example.com',
+  tel: '+145(2466)888',
+};
+
+//navbar component
+const navContainer = {
+  logo: '',
+  navItems: ['home', 'services', 'company', 'pages', 'news', 'contact'],
+};
+
+//landing page info
+const landingPageInfo = {
+  title: 'Shipping Products Worldwide',
+  text: 'with 100% Customer Satisfaction',
+};
+
+const aboutPageInfo = {
+  title: 'transtics shipping corp',
+  header: 'Taking care of you and your business all the way',
+  text: 'Solving your supply chain needs from end to end taking the complexity out of container shipping. We are at the forefront of developing innovation',
+  card: [
+    {
+      icon: '../../assets/icons/cargo-container-crane.svg',
+      cardInfo:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, eius? Natus magni ab dignissimos officiis. Amet nam ad odio id fugiat voluptatum?',
+    },
+    {
+      icon: '../../assets/icons/cruise.svg',
+      cardInfo:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, eius? Natus magni ab dignissimos officiis. Amet nam ad odio id fugiat voluptatum?',
+    },
+  ],
+  backgroundImg: '../../assets/map-mercator.svg',
+};
+
+const logisticSectionInfo = {
+  header: 'Logistics Services',
+  text: 'Solving your supply chain needs from end to end taking the complexity out of container shipping. We are at the forefront of developing innovation',
+  tabGroup: [
+    {
+      icon:
+    }
+  ]
+};
