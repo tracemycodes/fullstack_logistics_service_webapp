39a790a3b43432a86ec7eab70e8f5ca0d7bb2656
Agba Emmanuel <agbaayoh@gmail.com>
2022-06-09 08:39:40 -0700

    admin dashboard context api implemented

---
+++ src/context/types.js
@@ -0,0 +1,8 @@
+export const ADD_GOODS = 'ADD_GOODS';
+export const GET_GOODS = 'GET_GOODS';
+export const DELETE_GOODS = 'DELETE_GOODS';
+export const CURRENT_GOODS = 'CURRENT_GOODS';
+export const UPDATE_GOODS = 'UPDATE_GOODS';
+export const FILTER_GOODS = 'FILTER_GOODS';
+export const CLEAR_FILTER = 'CLEAR_FILTER';
+export const GOODS_ERROR = 'GOODS_ERROR';
